7a7a08501af4797c4c64ffe19cf4fd59cb9b4389
nporocohv <nporochov@emdev.ru>
2025-05-27 21:03:33 +0700

    remove HomeController and add Validation

--- src/main/java/ru/porochov/tacocloud/controller/DesignTacoController.java
+++ src/main/java/ru/porochov/tacocloud/controller/DesignTacoController.java
@@ -1,8 +1,10 @@
 package ru.porochov.tacocloud.controller;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,7 +63,11 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    private String processingTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+    private String processingTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
         tacoOrder.addTaco(taco);
 
         log.info("Processing taco: {}", taco);
